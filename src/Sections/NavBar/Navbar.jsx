44c90a08520753cb68984272168e11296a401386
Fezard Léo <leo.fezard33@gmail.com>
2023-02-26 20:25:39 +0100

    [ADD] mobile menu

--- src/Sections/NavBar/Navbar.jsx
+++ src/Sections/NavBar/Navbar.jsx
@@ -1,8 +1,17 @@
 import { useState } from "react"
 import Logo from "../../assets/logo_fabius_02.png"
+import MobileLogo from "../../assets/logo_fabius_02 (1).png"
+import BurgerMenu from "../../assets/burger-menu-svgrepo-com (1).svg"
+import Hamburger from 'hamburger-react'
 
 export default function Navbar() {
   const [currentSection, setCurrentSection] = useState()
+  const [mobileModal, setMobileModal] = useState(false)
+
+  const handleClick = () => {
+    console.log(mobileModal)
+    setMobileModal(!mobileModal)
+  }
 
   document.addEventListener("scroll", () => {
     const sections = document.querySelectorAll('section');
@@ -25,7 +34,7 @@ export default function Navbar() {
   })
 
   return (
-    <nav className="navbar-container">
+    <><nav className="navbar-container">
       <img src={Logo} alt="" />
       <p className={currentSection === "header" ? "active-section" : ""}><a href="/#header">HOME</a></p>
       <p className={currentSection === "about" ? "active-section" : ""}><a href="/#about">ABOUT</a></p>
@@ -41,5 +50,21 @@ export default function Navbar() {
         <p>EXTRA PAGES</p>
       </div>
     </nav>
+    <nav className="mobile-navbar-container">
+      <img src={MobileLogo} alt="" />
+      <Hamburger className="mobile-navbar-container__burger" color="white" toggled={mobileModal} toggle={setMobileModal} />
+    </nav>
+    <nav className={mobileModal ? "active-mobile" : "unactive-mobile"}>
+      <p style={{marginTop: "100px"}} className={currentSection === "header" ? "active-section" : ""}><a href="/#header">HOME</a></p>
+      <p className={currentSection === "about" ? "active-section" : ""}><a href="/#about">ABOUT</a></p>
+      <p className={currentSection === "services" ? "active-section" : ""}><a href="/#services">SERVICES</a> </p>
+      <p className={currentSection === "portfolio" ? "active-section" : ""}><a href="/#portfolio">PORTFOLIO</a></p>
+      <p className={currentSection === "skills" ? "active-section" : ""}><a href="/#skills">SKILLS</a></p>
+      <p className={currentSection === "experience" ? "active-section" : ""}><a href="/#experience">EXPERIENCE</a></p>
+      <p className={currentSection === "blog" ? "active-section" : ""}> <a href="/#blog">BLOG</a></p>
+      <p className={currentSection === "testimonial" ? "active-section" : ""}> <a href="/#testimonial">TESTIMONIAL</a></p>
+      <p className={currentSection === "contact" ? "active-section" : ""}> <a href="/#contact">CONTACT</a></p>
+      </nav>
+    </>
   )
 }
